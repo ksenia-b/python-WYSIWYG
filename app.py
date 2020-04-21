507d4cea68d08dd0b8a4f114c6dc2a366da7f74d
Oksana <ksenia.barshchyk@yandex.ru>
2020-04-21 11:08:15 +0300

    added display.html, bleach

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import bleach
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+class EditorData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    html = db.Column(db.Text)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        cleaned_data = bleach.clean(request.form.get('editordata'), tags = bleach.sanitizer.ALLOWED_TAGS + ['div', 'br'])
+        new_data = EditorData(html=cleaned_data)
+        db.session.add(new_data)
+        db.session.commit()
+        print('Posted Data')
+
+        return 'Posted Data' #redirect(url_for('index'))
+
     return render_template('index.html')
+
+
+@app.route('/display/<int:id>')
+def display(id):
+    data = EditorData.query.get(id)
+    print(data.html)
+    return render_template('display.html', data=data.html)
